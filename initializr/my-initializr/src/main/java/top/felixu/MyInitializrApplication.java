7ed5d689496049dd81408ee0129a00835e16c962
FelixXu <xufei@xiaoantimes.com>
2017-11-05 22:05:45 +0800

    spring initializr

---
+++ initializr/my-initializr/src/main/java/top/felixu/MyInitializrApplication.java
@@ -0,0 +1,12 @@
+package top.felixu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyInitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyInitializrApplication.class, args);
+	}
+}
---
+++ initializr/my-initializr/src/test/java/top/felixu/MyInitializrApplicationTests.java
@@ -0,0 +1,16 @@
+package top.felixu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyInitializrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
